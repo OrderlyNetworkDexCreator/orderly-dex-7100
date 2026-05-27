b430aa1365191f8e5b299103b672e020be56d34d
Mario Reder <mario.reder@pm.me>
2026-05-27 05:20:18 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/orderly-dex-7100/",
+  "VITE_SEO_SITE_URL": "https://169.254.169.254.xip.io",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
